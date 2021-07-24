26bc001a9c6f07bc897cd2baa47eaca5e79e3f07
RD <81473475+nylesor28@users.noreply.github.com>
2021-07-24 16:18:09 -0400

    generate webpage

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length)
+const fs = require('fs')
+const generatePage = require('./src/page-template.js')
 
-const printProfileData = profileDataArr=>{
+const profileDataArgs = process.argv.slice(2)
+const [name,github] = profileDataArgs;
+
+
+
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
   
-    profileDataArr.forEach(profileItem=> console.log(profileItem))
-
-}
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
